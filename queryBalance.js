8b6a43f60e4726191bb7d772feea7208a80ee542
Michael Templeton <michaeltempletonusa@gmail.com>
2025-02-11 00:20:54 +0000

    have tested against resolver.eth to verify this is working, quicknode.eth fails on Holesky and Sepolia

--- queryBalance.js
+++ queryBalance.js
@@ -13,25 +13,26 @@ const RPCUrls = {
 const ensName = process.env.ENS_NAME;
 
 const getLatestBalanceAndDetails = async (network) => {
- const web3 = new Web3(RPCUrls[network]); // quickly pass in the RPC URL for a given network to access functions
+    const web3 = new Web3(RPCUrls[network]); // quickly pass in the RPC URL for a given network to access functions
 
- try {
-    const ownerAddress = await web3.eth.ens.getAddress(ensName); // resolve the ensName
-    if (!ownerAddress || ownerAddress === "0x0000000000000000000000000000000000000000") {
-        return console.error("Invalid ENS Owner Address");
+    try {
+        const ownerAddress = await web3.eth.ens.getAddress(ensName); // resolve the ensName
+        console.log("ownerAddress", ownerAddress)
+        if (!ownerAddress || ownerAddress === "0x0000000000000000000000000000000000000000") {
+            return console.error("Invalid ENS Owner Address");
+        }
+
+        // run the appropriate methods for getting: latest block number, latest balance, and latest transaction count for the address on the given network
+        const latestBlockNumber = await web3.eth.getBlockNumber();
+        const balanceWei = await web3.eth.getBalance(ownerAddress, latestBlockNumber);
+        const balanceEther = web3.utils.fromWei(balanceWei, "ether");
+        const transactionCount = await web3.eth.getTransactionCount(ownerAddress)
+        console.log(`Latest block number: ${latestBlockNumber}`);
+        console.log(`Balance of address ${ownerAddress} on ${network}: ${balanceEther} ETH`);
+        console.log(`Transaction Count: ${transactionCount}`);
+    } catch (err) {
+        console.error(`Error querying ${network}: ${err.message}`);
     }
-
-    // run the appropriate methods for getting: latest block number, latest balance, and latest transaction count for the address on the given network
-    const latestBlockNumber = await web3.eth.getBlockNumber();
-    const balanceWei = await web3.eth.getBalance(ownerAddress, latestBlockNumber);
-    const balanceEther = web3.utils.fromWei(balanceWei, "ether");
-    const transactionCount = await web3.eth.getTransactionCount(ownerAddress)
-    console.log(`Latest block number: ${latestBlockNumber}`);
-    console.log(`Balance of address ${ownerAddress} on ${network}: ${balanceEther} ETH`);
-    console.log(`Transaction Count: ${transactionCount}`);
- } catch (err) {
-    console.error(`Error querying ${network}: ${err.message}`);
- }
 }
 
 (async () => {
